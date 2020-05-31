97f7fa58f6287128031112cc2b819ba04f5cd62d
Kaiwu Li <kai.li@rea-group.com>
2020-05-31 18:50:58 +1000

    app: get fragment via router

--- app/src/main/java/com/example/routerdemo/MainActivity.kt
+++ app/src/main/java/com/example/routerdemo/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.routerdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.basemodule.context
+import com.example.routerservicemodule.getAppConfigReceiverService
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -16,17 +19,32 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation?.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.menu_search -> {
+                    context.getAppConfigReceiverService()?.getSearchEntryFragment()?.let {
+                        openFragment(it)
+                    }
                     true
                 }
                 R.id.menu_auth -> {
+                    context.getAppConfigReceiverService()?.getAuthEntryFragment()?.let {
+                        openFragment(it)
+                    }
                     true
                 }
                 R.id.menu_bolt -> {
-
+                    context.getAppConfigReceiverService()?.getBoltEntryFragment()?.let {
+                        openFragment(it)
+                    }
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
