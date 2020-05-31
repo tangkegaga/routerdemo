07976399639ef757b45e9323a509d79b80da0f6c
Kaiwu Li <kai.li@rea-group.com>
2020-05-31 16:54:18 +1000

    register services to router

--- app/src/main/java/com/example/routerdemo/routerservice/AppConfigRouterServiceLifeCycle.kt
+++ app/src/main/java/com/example/routerdemo/routerservice/AppConfigRouterServiceLifeCycle.kt
@@ -1,12 +1,15 @@
 package com.example.routerdemo.routerservice
 
 import android.app.Application
+import com.example.authmodule.routerservice.AuthProviderServiceImp
+import com.example.authmodule.routerservice.IAuthProviderService
 import com.example.basemodule.router.IModuleConfig
 import com.example.routerservicemodule.app.AppConfigReceiverServiceLifeCycle
+import com.example.routerservicemodule.app.IAppConfigProviderService
 
 class AppConfigRouterServiceLifeCycle(application: Application) : AppConfigReceiverServiceLifeCycle(application) {
     override fun onCreate(config: IModuleConfig) {
         super.onCreate(config)
-
+        config.registerService(IAppConfigProviderService::class.java, AppConfigProviderServiceImp::class.java)
     }
 }
--- app/src/main/java/com/example/routerdemo/routerservice/ModuleLifeCycleManager.kt
+++ app/src/main/java/com/example/routerdemo/routerservice/ModuleLifeCycleManager.kt
@@ -3,13 +3,16 @@ package com.example.routerdemo.routerservice
 import android.app.Application
 import com.example.basemodule.router.BaseModuleLifeCycleManager
 import com.example.routerservicemodule.auth.AuthRouterServicesLifeCycle
+import com.example.routerservicemodule.search.SearchRouterServicesLifeCycle
 
 
 internal class ModuleLifeCycleManager(application: Application) :
     BaseModuleLifeCycleManager(application) {
 
     init {
+        moduleLifeCycleList.add(AppConfigRouterServiceLifeCycle(application))
         moduleLifeCycleList.add(AuthRouterServicesLifeCycle(application))
+        moduleLifeCycleList.add(SearchRouterServicesLifeCycle(application))
     }
 
 }
