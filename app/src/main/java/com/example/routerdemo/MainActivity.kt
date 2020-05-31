18982fe702c8adb1740fd84bb333ca5db09f92be
Kaiwu Li <kai.li@rea-group.com>
2020-05-31 16:50:40 +1000

    create empty bottom bar

--- app/src/main/java/com/example/routerdemo/MainActivity.kt
+++ app/src/main/java/com/example/routerdemo/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.routerdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+var bottomNavigation: BottomNavigationView? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation?.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_search -> {
+                    true
+                }
+                R.id.menu_auth -> {
+                    true
+                }
+                R.id.menu_bolt -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
