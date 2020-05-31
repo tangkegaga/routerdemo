a7239a1719c2c8827bb3c902a1cef5f2456a3858
Kaiwu Li <kai.li@rea-group.com>
2020-05-31 13:23:08 +1000

    launch/hook router service in base app module

---
+++ basemodule/src/main/java/com/example/basemodule/BaseModuleApplication.kt
@@ -0,0 +1,32 @@
+package com.example.basemodule
+
+import android.app.Application
+import com.example.basemodule.router.BaseModuleLifeCycleManager
+import com.example.basemodule.router.IModuleConfig
+
+lateinit var context: BaseModuleApplication
+
+fun getAppContext(): BaseModuleApplication {
+    return context
+}
+
+abstract class BaseModuleApplication : Application() {
+
+    var lifeCycleManager: BaseModuleLifeCycleManager? = null
+    abstract fun initLifeCycleManager(): BaseModuleLifeCycleManager
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+        lifeCycleManager = initLifeCycleManager()
+        lifeCycleManager!!.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        lifeCycleManager?.onDestroy()
+    }
+
+    fun getModuleConfig(): IModuleConfig = lifeCycleManager!!.getModuleConfig()
+}
+
